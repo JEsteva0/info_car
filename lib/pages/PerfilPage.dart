cc9f409a61cf241f651a243e7f96b72d304000b8
Pedro Sawczuk <pedrosawczuk53@gmail.com>
2023-12-01 18:02:45 -0400

    perfil

--- lib/pages/PerfilPage.dart
+++ lib/pages/PerfilPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class PagePerfil extends StatefulWidget {
-  const PagePerfil({super.key});
+  const PagePerfil({Key? key}) : super(key: key);
 
   @override
   State<PagePerfil> createState() => _PagePerfilState();
@@ -11,7 +11,50 @@ class _PagePerfilState extends State<PagePerfil> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Perfil'),
+      appBar: AppBar(
+        title: Text('Perfil'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/perfil.jpg'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Sova',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Email: sova@gmail.com',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Telefone: (123) 456-7890',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
 }
